3c34f086f403670ec6ee59bbee312050bff155eb
novakvova <novakvova@gmail>
2022-12-24 20:53:02 +0200

    Test lib dotnet core

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Data;
+using DAL.Data.Entities;
+using System;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EFAppContext context = new EFAppContext();
+            CategoryEntity cat = new CategoryEntity()
+            {
+                Name = "Смачні стави",
+                DateCreated = DateTime.Now
+            };
+            context.Categories.Add(cat);
+            context.SaveChanges();
+            Console.WriteLine("Create id {0}", cat.Id);
+        }
+    }
+}
