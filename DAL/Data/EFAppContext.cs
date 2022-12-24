74a6822aab24a4f27a0339e5e747c1f9f98ad9bd
novakvova <novakvova@gmail>
2022-12-24 20:26:00 +0200

    Database simple table tblCategory

---
+++ DAL/Data/EFAppContext.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Data
+{
+    public class EFAppContext : DbContext
+    {
+        public DbSet<CategoryEntity> Categories { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseNpgsql("Server=localhost;Port=5432;Database=teambv121;User Id=postgres;Password=123456;");
+        }
+    }
+}
---
+++ DAL/Data/Entities/CategoryEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DAL.Data.Entities
+{
+    [Table("tblCategories")]
+    public class CategoryEntity : BaseEntity<int>
+    {
+        [Required, StringLength(255)]
+        public string Name { get; set; }
+        [StringLength(255)]
+        public string Image { get; set; }
+    }
+}
