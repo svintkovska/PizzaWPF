284e37f9ac77036920c15fcb5ffab35e478a47ff
svintkovska <tanyasv97@gmail.com>
2023-01-04 20:13:20 +0200

    add pages ViewModel, add CategoriesPage

--- PizzaUI/MainWindow.xaml.cs
+++ PizzaUI/MainWindow.xaml.cs
@@ -24,8 +24,13 @@ namespace PizzaUI
         public MainWindow()
         {
             InitializeComponent();
-            CategoryWindow cw = new CategoryWindow();
-            cw.ShowDialog();
+            //CategoryWindow cw = new CategoryWindow();
+            //cw.ShowDialog();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
