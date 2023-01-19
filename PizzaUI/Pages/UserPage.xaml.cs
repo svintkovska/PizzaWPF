b0631afc55c4e738a0446a078b9ecf24a5d6e9b1
svintkovska <tanyasv97@gmail.com>
2023-01-20 00:03:20 +0200

    add UI for OrderHistory

--- PizzaUI/Pages/UserPage.xaml.cs
+++ PizzaUI/Pages/UserPage.xaml.cs
@@ -2,6 +2,8 @@
 using BLL.Services;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -24,6 +26,8 @@ namespace PizzaUI.Pages
     {
         UserDTO user = new UserDTO();
         UserService _userService = new UserService();
+        ProductService _productService = new ProductService();
+        OrderService _orderService = new OrderService();
         public UserPage()
         {
             InitializeComponent();
@@ -34,7 +38,10 @@ namespace PizzaUI.Pages
                 userLastName_tb.Text = user.LastName;
                 userEmail_tb.Text = user.Email;
                 userPhone_tb.Text = user.Phone;
+
+                FillOrderHistory();
             }
+
         }
         private readonly Regex validateEmailRegex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
         private readonly Regex validatePhoneNumberRegex = new Regex("^[+][3][8][0][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$");
@@ -108,6 +115,133 @@ namespace PizzaUI.Pages
             return sBuilder.ToString();
         }
 
-       
+
+        //<ListBoxItem Height = "100" Background="DarkRed" Focusable="False">
+        //                    <Grid >
+        //                        <Grid.ColumnDefinitions>
+        //                            <ColumnDefinition Width = "5*" />
+        //                            < ColumnDefinition />
+        //                            < ColumnDefinition />
+        //                            < ColumnDefinition />
+        //                        </ Grid.ColumnDefinitions >
+        //                        < Grid.RowDefinitions >
+        //                            < RowDefinition Height="0.5*"/>
+        //                            <RowDefinition/>
+        //                        </Grid.RowDefinitions>
+        //                        <Label Grid.Column="0" Grid.Row= "0" FontSize= "17" Margin= "10 0 20 0" > Date:</Label>
+        //                        <Label Grid.Column= "1" Grid.Row= "0"  FontSize= "17" Margin= "0 0 20 0" > Total price: </Label>
+        //                        <Label Grid.Column= "2" Grid.Row= "0" FontSize= "17" Margin= "0 0 20 0" > Amount: </Label>
+        //                        <Label Grid.Column= "3" Grid.Row= "0"  FontSize= "17" Margin= "0" > Name: </Label>
+
+        //                        <TextBlock Grid.Column= "0" Grid.Row= "1" Foreground= "Black" Margin= "10 0" HorizontalAlignment= "Center" VerticalAlignment= "Center" > 19.01.23 15:00:00</TextBlock>
+        //                        <TextBlock Grid.Column= "1" Grid.Row= "1" Foreground= "Black" Margin= "0" HorizontalAlignment= "Center" VerticalAlignment= "Center" > 1250 </ TextBlock >
+        //                        < TextBlock Grid.Column= "2" Grid.Row= "1" Foreground= "Black" Margin= "0" HorizontalAlignment= "Center" VerticalAlignment= "Center" > 19 </ TextBlock >
+        //                        < Image Grid.Column= "3" Grid.Row= "2" Width= "70" Height= "70"
+        //                               Source= "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg" ></ Image >
+        //                    </ Grid >
+        //                </ ListBoxItem >
+         private async void FillOrderHistory()
+         {
+            List<OrderDTO> history =  _orderService.GetOrderHistory(user.Id);
+
+            List<OrderItemDTO> orderItems = new List<OrderItemDTO>();
+            foreach (var order in history)
+            {
+                foreach(var item in _orderService.GetOrderItemsByOrderId(order.Id))
+                {
+                    orderItems.Add(item);
+                }
+            }
+
+            foreach (var item in orderItems)
+            {
+                var product = await _productService.Find(item.ProductId);
+
+
+
+                ListBoxItem listBoxItem = new ListBoxItem() { Height = 100, Background = Brushes.DarkRed, Focusable = false };
+                Grid grid = new Grid();
+
+                grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(0.5, GridUnitType.Star) });
+                grid.RowDefinitions.Add(new RowDefinition());
+
+                grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(5.0, GridUnitType.Star) });
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+
+                Label labelDate = new Label() { Content = "Date:", FontSize = 17, Margin = new Thickness(10, 0, 20, 0) };
+                Label labelPrice = new Label() { Content = "Price:", FontSize = 17, Margin = new Thickness(0, 0, 20, 0) };
+                Label labelAmount = new Label() { Content = "Amount:", FontSize = 17, Margin = new Thickness(0, 0, 20, 0) };
+                Label labelName = new Label() { Content = product.Name, FontSize = 17, Margin = new Thickness(0, 0, 20, 0) };
+
+                TextBlock dateText = new TextBlock() { Text = item.DateCreated.ToString(), Foreground = Brushes.Black, Margin = new Thickness(10, 0, 10, 0), 
+                    HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center };
+                TextBlock priceText = new TextBlock(){ Text = item.Price.ToString(), Foreground = Brushes.Black, Margin = new Thickness(0),
+                    HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center };
+                TextBlock amountText = new TextBlock() {Text = item.Count.ToString(), Foreground = Brushes.Black,  Margin = new Thickness(0),
+                    HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center };
+
+                Image img = new Image() { Width = 70, Height = 60 };
+                BitmapImage bmp = new BitmapImage();
+                string someUrl = await _productService.GetImg(item.ProductId);
+                using (var webClient = new WebClient())
+                {
+                    byte[] imageBytes = webClient.DownloadData(someUrl);
+                    bmp = ToBitmapImage(imageBytes);
+                }
+                img.Source = bmp;
+
+                Grid.SetRow(labelDate, 0);
+                Grid.SetColumn(labelDate, 0);
+                grid.Children.Add(labelDate);
+                Grid.SetRow(labelPrice, 0);
+                Grid.SetColumn(labelPrice, 1);
+                grid.Children.Add(labelPrice);
+                Grid.SetRow(labelAmount, 0);
+                Grid.SetColumn(labelAmount, 2);
+                grid.Children.Add(labelAmount);
+                Grid.SetRow(labelName, 0);
+                Grid.SetColumn(labelName, 3);
+                grid.Children.Add(labelName);
+                Grid.SetRow(dateText, 1);
+                Grid.SetColumn(dateText, 0);
+                grid.Children.Add(dateText);
+                Grid.SetRow(priceText, 1);
+                Grid.SetColumn(priceText, 1);
+                grid.Children.Add(priceText);
+                Grid.SetRow(amountText, 1);
+                Grid.SetColumn(amountText, 2);
+                grid.Children.Add(amountText);
+                Grid.SetRow(img, 1);
+                Grid.SetColumn(img, 3);
+                grid.Children.Add(img);
+
+                listBoxItem.Content = grid;
+                orderHistoryListBox.Items.Add(listBoxItem);
+
+            }
+
+           
+        }
+
+        private BitmapImage ToBitmapImage(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+
+                BitmapImage img = new BitmapImage();
+                img.BeginInit();
+                img.CacheOption = BitmapCacheOption.OnLoad;
+                img.StreamSource = ms;
+                img.EndInit();
+
+                if (img.CanFreeze)
+                {
+                    img.Freeze();
+                }
+                return img;
+            }
+        }
     }
 }
