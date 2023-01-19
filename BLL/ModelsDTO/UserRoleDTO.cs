54351dd953350f2fe58cb2a982d7a5ccd5be216c
svintkovska <tanyasv97@gmail.com>
2023-01-19 17:44:46 +0200

    add SuperAdmin. add possibility to set a user as Admin

--- BLL/ModelsDTO/UserRoleDTO.cs
+++ BLL/ModelsDTO/UserRoleDTO.cs
@@ -1,12 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace BLL.ModelsDTO
 {
-    public class UserRoleDTO
+    public class UserRoleDTO: INotifyPropertyChanged
     {
-        public int UserId { get; set; }
-        public int RoleId { get; set; }
+        private int _userId;
+        public int UserId
+        {
+            get
+            {
+                return _userId;
+            }
+            set
+            {
+                if (_userId != value)
+                {
+                    _userId = value;
+                    NotifyPropertyCahnged(nameof(UserId));
+                }
+            }
+        }
+
+        private int _roleId;
+        public int RoleId
+        {
+            get
+            {
+                return _roleId;
+            }
+            set
+            {
+                if (_roleId != value)
+                {
+                    _roleId = value;
+                    NotifyPropertyCahnged(nameof(RoleId));
+                }
+            }
+        }
+
+        public virtual UserDTO User { get; set; }
+        public virtual RoleDTO Role { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyCahnged(string propName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+
     }
 }
---
+++ PizzaUI/Pages/SuperAdminPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using BLL.ModelsDTO;
+using BLL.Services;
+using PizzaUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PizzaUI.Pages
+{
+    /// <summary>
+    /// Interaction logic for SuperAdminPage.xaml
+    /// </summary>
+    public partial class SuperAdminPage : Page
+    {
+        UserRolesService _userRolesService = new UserRolesService();
+        RoleService _roleService = new RoleService();
+        public SuperAdminPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void setAsAdminBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var roles = _roleService.GetAll();
+            int adminRoleId = roles.Where(r => r.Name == "Admin").FirstOrDefault().Id;
+            var user = allUsersCombobox.SelectedItem as UserDTO;
+            var userRole = new UserRoleDTO()
+            {
+                RoleId = adminRoleId,
+                UserId = user.Id
+            };
+            await _userRolesService.Create(userRole);
+
+            var cm = DataContext as CategoriesVM;
+            cm.GetUsers.Remove(user);
+            cm.GetAdmins.Add(user);
+            allUsersCombobox.SelectedIndex = -1;
+        }
+    }
+}
