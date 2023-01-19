949f7136d84f23ff7b1ff8bba990793dd16ecac2
svintkovska <tanyasv97@gmail.com>
2023-01-19 20:21:29 +0200

    add repositories for Basket, Order and OrderItem

--- DAL/Interfaces/IOrderRepository.cs
+++ DAL/Interfaces/IOrderRepository.cs
@@ -2,12 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DAL.Interfaces
 {
-    public interface IUserRepository : IGenericRepository<UserEntity, int>
+    public interface IOrderRepository : IGenericRepository<OrderEntity, int>
     {
-      
+
     }
 }
